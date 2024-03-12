e8ec24df8b1960f431cafa2e43666058ca5e9cca
hojunin <dlsghwns@naver.com>
2024-03-12 14:48:12 +0900

    feat: 파일 업로드 인터렉션 구현(toast)

--- src/components/file-upload-button.tsx
+++ src/components/file-upload-button.tsx
@@ -2,11 +2,30 @@
 import React, { ChangeEvent } from 'react';
 import { Button } from './ui/button';
 import { uploadFile } from '@/api/file-upload';
+import { useToast } from './ui/use-toast';
 
 const FileUploadButton = () => {
+  const { toast } = useToast();
   const handleFile = async (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files[0];
-    const uploadedUrl = await uploadFile(file);
+    try {
+      const uploadedUrl = await uploadFile(file);
+      toast({
+        title: '업로드 성공',
+        description: `${file.name} 업로드 성공`,
+        action: (
+          <Button onClick={() => navigator.clipboard.writeText(uploadedUrl)}>
+            주소 복사
+          </Button>
+        ),
+      });
+    } catch (error) {
+      toast({
+        title: '문제가 발생했어요',
+        description: error?.message,
+        variant: 'destructive',
+      });
+    }
   };
   return (
     <Button>
