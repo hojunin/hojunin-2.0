32cfe09ec4855cfe7bbcdd6748a8484b0a7f527c
hojunin <dlsghwns@naver.com>
2024-03-19 01:54:24 +0900

    feat: notfound 컴포넌트 수정, 게시글 리스트 api 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,8 @@
+import CommonError from '@/components/common/common-error';
 import React from 'react';
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return <CommonError message="페이지를 찾을 수 없어요" />;
 };
 
 export default NotFoundPage;
