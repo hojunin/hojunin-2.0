a3bd47f0d3917fc8738bce0ea88f10c5a2da99e9
hojunin <dlsghwns@naver.com>
2024-03-18 18:34:26 +0900

    chore: 타입 및 안쓰는 코드 제거

--- src/components/contents/contents-detail/index.tsx
+++ src/components/contents/contents-detail/index.tsx
@@ -1,40 +1,27 @@
 import React from 'react';
-import { fetcher } from '@/api/fetcher';
-import CommonError from '@/components/common/common-error';
 import { useMDXComponents } from '@/mdx-components';
-import { ValueOf } from '@/types/common';
-import { ContentsCategory, PostListItemInterface } from '@/types/contents';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import remarkGfm from 'remark-gfm';
 import prism from 'rehype-prism-plus';
 import { parseCodeSnippet } from '@/lib/mdx';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 
 interface Props {
-  category: ValueOf<typeof ContentsCategory>;
-  slug: string;
+  content: string;
 }
 
-const ContentsDetail = async ({ category, slug }: Props) => {
+const ContentsDetail = async ({ content }: Props) => {
   const MDXComponents = useMDXComponents();
-  const contentDetail = await fetcher<PostListItemInterface>({
-    path: `contents/${category}/${slug}`,
-  });
 
-  if (!contentDetail) {
-    return <CommonError />;
-  }
   return (
     <div>
       <MDXRemote
         options={{
           mdxOptions: {
-            remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
+            remarkPlugins: [remarkGfm],
             rehypePlugins: [prism, parseCodeSnippet],
           },
         }}
-        source={contentDetail.body}
+        source={content}
         components={MDXComponents}
       />
     </div>
