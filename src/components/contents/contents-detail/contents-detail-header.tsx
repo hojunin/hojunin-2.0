631dcab86670ae8101271fb28fb8413e7125afbb
hojunin <dlsghwns@naver.com>
2024-03-18 18:44:19 +0900

    fix: view-count가 최신화되지 않는 오류 수정

--- src/components/contents/contents-detail/contents-detail-header.tsx
+++ src/components/contents/contents-detail/contents-detail-header.tsx
@@ -1,7 +1,9 @@
 import ContentsDetailBreadCrumb from '@/components/contents/contents-detail/bread-crumbs';
 import ContentsViewCount from '@/components/contents/contents-view-count';
+import { Skeleton } from '@/components/ui/skeleton';
 import { ValueOf } from '@/types/common';
 import { ContentsCategory } from '@/types/contents';
+import { EyeIcon } from 'lucide-react';
 import React, { Suspense } from 'react';
 
 interface Props {
@@ -14,7 +16,14 @@ const ContentsDetailHeader = ({ category, slug }: Props) => {
     <section>
       <ContentsDetailBreadCrumb currentCategory={category} slug={slug} />
 
-      <Suspense fallback={null}>
+      <Suspense
+        fallback={
+          <div className="flex items-center gap-x-2">
+            <EyeIcon width={16} height={16} color="#efefef" />
+            <Skeleton className="w-4 h-4" />
+          </div>
+        }
+      >
         <ContentsViewCount slug={slug} />
       </Suspense>
     </section>
