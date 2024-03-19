d5b6adcc318bfe3afa506644d808ec1582601e65
hojunin <dlsghwns@naver.com>
2024-03-19 13:56:37 +0900

    feat: 베스트 컨텐츠 리스트 컴포넌트 추가

--- src/components/home/best-contents/index.tsx
+++ src/components/home/best-contents/index.tsx
@@ -1,10 +1,35 @@
 import Typography from '@/components/common/typography';
+import BestContentsItem from '@/components/home/best-contents/best-contents-item';
+import { createClient } from '@/lib/supabase/server';
+import { PostListItemInterface } from '@/types/contents';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
 import React from 'react';
 
-const BestContents = () => {
+const BestContents = async () => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: contents, error } = await supabase.rpc('best_contents');
+
+  if (error) {
+    return null;
+  }
+
   return (
-    <section className="mt-4">
-      <Typography variant={'h2'}>베스트 컨텐츠</Typography>
+    <section className="my-6">
+      <div className="flex items-center justify-between mb-3">
+        <Typography variant={'h2'}>베스트 컨텐츠</Typography>
+
+        <Link href="/contents" className="text-muted-foreground">
+          더 보러가기
+        </Link>
+      </div>
+
+      <ul className="flex items-center gap-x-4">
+        {(contents as PostListItemInterface[]).map((content) => (
+          <BestContentsItem content={content} key={content.id} />
+        ))}
+      </ul>
     </section>
   );
 };
