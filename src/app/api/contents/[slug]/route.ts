3baebb530f8f8e1aa1682f0e79f654e307c5cc48
hojunin <dlsghwns@naver.com>
2024-03-19 20:24:28 +0900

    fix: API 스펙 변경(contents)

--- src/app/api/contents/[slug]/route.ts
+++ src/app/api/contents/[slug]/route.ts
@@ -1,9 +1,11 @@
 import { createClient } from '@/lib/supabase/server';
+import { ValueOf } from '@/types/common';
+import { ContentsCategory } from '@/types/contents';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 interface IParams {
-  category: string;
+  slug: string;
 }
 
 export async function GET(request: Request, { params }: { params: IParams }) {
@@ -13,7 +15,8 @@ export async function GET(request: Request, { params }: { params: IParams }) {
   const { data, error } = await supabase
     .from('contents')
     .select('*')
-    .eq('category', params.category);
+    .eq('slug', params.slug)
+    .single();
 
   if (error) {
     return NextResponse.error();
