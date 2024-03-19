ea8ed5579a2c7284a8223b60f36332cb967c252e
hojunin <dlsghwns@naver.com>
2024-03-19 20:29:44 +0900

    fix: category depth 수정

--- src/components/contents/post/post-list.tsx
+++ src/components/contents/post/post-list.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import PostListItem from './post-list-item';
-import CommonError from '../../common/common-error';
-import { cookies } from 'next/headers';
 import { createClient } from '@/lib/supabase/server';
+import { cookies } from 'next/headers';
+import CommonError from '@/components/common/common-error';
+import { ContentsTag } from '@/types/contents';
 
 interface Props {
-  category: string;
+  tag: ContentsTag;
 }
 
-const PostList = async ({ category }: Props) => {
+const PostList = async ({ tag }: Props) => {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-
   const { data: postList, error } = await supabase
     .from('contents')
-    .select(
-      'id, created_at, title, description, thumbnail, slug, category, status',
-    )
-    .eq('category', category);
+    .select('*')
+    .eq('tag', tag.id)
+    .order('created_at');
 
-  if (!postList || error) {
+  if (error || !postList) {
     return <CommonError message="데이터가 없어요" />;
   }
 
