773b2b5de8ade498849a6837ebfec797d17eb917
HOJUN IN <hojunin@hojunin-MacBook.local>
2024-06-02 18:45:12 +0900

    feat: 레이아웃 수정(헤더에 블러효과 추가)

--- src/components/common/global-naviagation-bar.tsx
+++ src/components/common/global-naviagation-bar.tsx
@@ -1,33 +1,32 @@
-import React from 'react';
+import React from "react";
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   navigationMenuTriggerStyle,
-} from '@/components/ui/navigation-menu';
+} from "@/components/ui/navigation-menu";
 
-import Link from 'next/link';
-import { DarkMode } from './dark-mode-button';
-import { cn } from '@/lib/utils';
-import UserInfoButton from '@/components/common/user-info-button';
-import Typography from '@/components/common/typography';
+import Link from "next/link";
+import { DarkMode } from "./dark-mode-button";
+import { cn } from "@/lib/utils";
+import UserInfoButton from "@/components/common/user-info-button";
 
 const MENU_LINKS = [
-  { label: '컨텐츠', link: '/contents' },
+  { label: "컨텐츠", link: "/contents" },
   {
-    label: '챌린지',
-    link: '/challenge',
+    label: "챌린지",
+    link: "/challenge",
   },
   {
-    label: '내 소개',
-    link: '/about',
+    label: "내 소개",
+    link: "/about",
   },
 ];
 
 const GlobalNavigationBar = () => {
   return (
-    <header className="fixed w-full bg-background opacity-80 flex items-center justify-between p-6 border-b z-50">
+    <header className="flex items-center justify-between p-6 z-50 sticky top-0 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex gap-x-7">
         <Link href="/">
           <nav className="text-2xl font-bold tracking-tight lg:text-3xl">
--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import React from 'react';
-import GlobalNavigationBar from './global-naviagation-bar';
-import Footer from './footer';
+import React from "react";
+import GlobalNavigationBar from "./global-naviagation-bar";
+import Footer from "./footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <GlobalNavigationBar />
 
-      <main className="flex flex-col py-24 min-h-screen my-0 mx-auto justify-between px-4 md:px-20 md:w-4/5 sm:px-40 sm:w-2/3">
+      <main className="flex flex-col min-h-screen my-0 mx-auto justify-between px-4 md:px-20 md:w-4/5 sm:px-40 sm:w-2/3">
         {children}
       </main>
 
