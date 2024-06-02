a8490df01453495fa00a201565d0b68cc31c8f7d
hojunin <dlsghwns@naver.com>
2024-06-02 22:35:03 +0900

    prettier 플러그인과 설정 추가

--- src/components/common/global-naviagation-bar.tsx
+++ src/components/common/global-naviagation-bar.tsx
@@ -1,11 +1,11 @@
 'use client';
 import React from 'react';
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 
 import Link from 'next/link';
@@ -14,51 +14,47 @@ import { cn } from '@/lib/utils';
 import UserInfoButton from '@/components/common/user-info-button';
 
 const MENU_LINKS = [
-  { label: '컨텐츠', link: '/contents' },
-  {
-    label: '챌린지',
-    link: '/challenge',
-  },
-  {
-    label: '내 소개',
-    link: '/about',
-  },
+	{ label: '컨텐츠', link: '/contents' },
+	{
+		label: '챌린지',
+		link: '/challenge',
+	},
+	{
+		label: '내 소개',
+		link: '/about',
+	},
 ];
 
 const GlobalNavigationBar = () => {
-  return (
-    <header className="flex items-center justify-between p-6 z-50 sticky top-0 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="flex gap-x-7">
-        <Link href="/">
-          <nav className="text-2xl font-bold tracking-tight lg:text-3xl">
-            HJINN
-          </nav>
-        </Link>
+	return (
+		<header className="sticky top-0 z-50 flex w-full items-center justify-between border-b border-border/40 bg-background/95 p-6 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+			<div className="flex gap-x-7">
+				<Link href="/">
+					<nav className="text-2xl font-bold tracking-tight lg:text-3xl">HJINN</nav>
+				</Link>
 
-        <NavigationMenu>
-          <NavigationMenuList>
-            {MENU_LINKS.map(({ label, link }) => (
-              <NavigationMenuItem key={`${label}-${link}`}>
-                <Link href={link} legacyBehavior passHref>
-                  <NavigationMenuLink
-                    className={cn(navigationMenuTriggerStyle())}
-                  >
-                    {label}
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
+				<NavigationMenu>
+					<NavigationMenuList>
+						{MENU_LINKS.map(({ label, link }) => (
+							<NavigationMenuItem key={`${label}-${link}`}>
+								<Link href={link} legacyBehavior passHref>
+									<NavigationMenuLink className={cn(navigationMenuTriggerStyle())}>
+										{label}
+									</NavigationMenuLink>
+								</Link>
+							</NavigationMenuItem>
+						))}
+					</NavigationMenuList>
+				</NavigationMenu>
+			</div>
 
-      <div className="flex gap-x-3 ">
-        <UserInfoButton />
+			<div className="flex gap-x-3">
+				<UserInfoButton />
 
-        <DarkMode />
-      </div>
-    </header>
-  );
+				<DarkMode />
+			</div>
+		</header>
+	);
 };
 
 export default GlobalNavigationBar;
