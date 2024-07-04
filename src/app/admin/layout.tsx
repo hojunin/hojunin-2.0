1f01c0ae958467a12099433824d8817cb93e9356
인호준 <hjinn@29cm.co.kr>
2024-07-05 00:09:39 +0900

    clarity 제거

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,15 @@
+import SupabaseProvider from '@/context/supabase-provider'
+import React from 'react'
+
+const AdminLayout = ({children}: {children:React.ReactNode}) => {
+  return (
+    <div>
+        <SupabaseProvider>
+
+        {children}
+        </SupabaseProvider>
+        </div>
+  )
+}
+
+export default AdminLayout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,30 +23,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  console.log("RERENDER")
   return (
     <html lang="en">
-      <head>
-        <Script
-          id="clarity"
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "lh4vfz29xp");
-        `,
-          }}
-        />
-      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" enableSystem defaultTheme="system">
             <Providers>
-            {/* <SupabaseProvider> */}
             <Layout>{children}</Layout>
-            {/* </SupabaseProvider> */}
         <Toaster />
             </Providers>
         </ThemeProvider>
