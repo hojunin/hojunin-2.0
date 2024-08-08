04961cf6d63c65feee2f501d93edab339733aed5
인호준 <hjinn@29cm.co.kr>
2024-08-09 01:24:20 +0900

    OG 자동생성 홈

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,11 +1,10 @@
-import { fetchMetaData } from '@/api/contents';
 import { ImageResponse } from 'next/og';
 
 // Route segment config
 export const runtime = 'edge';
 
 // Image metadata
-export const alt = '컨텐츠 | HJINN';
+export const alt = '홈 | HJINN';
 export const size = {
 	width: 1200,
 	height: 630,
@@ -14,12 +13,7 @@ export const size = {
 export const contentType = 'image/png';
 
 // Image generation
-export default async function Image({ params }: { params: { slug: string } }) {
-	const content = await fetchMetaData(params.slug);
-	if (!content) {
-		return new Response('Not found', { status: 404 });
-	}
-	const { title, description } = content;
+export default async function Image() {
 	const nanumHand = fetch(
 		'https://lnwblzacktgzeiihvxtu.supabase.co/storage/v1/object/sign/asset/nanum.ttf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhc3NldC9uYW51bS50dGYiLCJpYXQiOjE3MjMxMzI4NTQsImV4cCI6MjAzODQ5Mjg1NH0.u_vxn3BzZFe0M8ZS9VM5S7Vw4m7YZ9L8BWCIPsDqe4M&t=2024-08-08T16%3A00%3A55.228Z',
 	).then(res => res.arrayBuffer());
@@ -40,9 +34,7 @@ export default async function Image({ params }: { params: { slug: string } }) {
 					borderRadius: '8px',
 				}}
 			>
-				<h1 style={{ fontSize: 128, fontWeight: 'bold' }}>{title}</h1>
-				<br />
-				<p style={{ fontSize: 64 }}>{description?.slice(0, 40)}</p>
+				<h1 style={{ fontSize: 128, fontWeight: 'bold' }}>HJINN</h1>
 			</div>
 		),
 		{
