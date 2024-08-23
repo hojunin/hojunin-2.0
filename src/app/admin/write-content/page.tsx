dd08a97c8768459e6aa9cf0cc912fcc177a6bdc4
인호준 <hjinn@29cm.co.kr>
2024-08-24 02:17:11 +0900

    나 말고 글 못쓰게 막아

--- src/app/admin/write-content/page.tsx
+++ src/app/admin/write-content/page.tsx
@@ -1,7 +1,16 @@
+'use client';
 import WriteEditor from '@/components/admin/write-content/write-editor';
 import React from 'react';
+import { useAdminPage } from '../admin-page.hooks';
+import { useRouter } from 'next/navigation';
 
 const WriteContentPage = () => {
+	const { isInitialized, user } = useAdminPage();
+	const router = useRouter();
+	if (isInitialized && (!user || user?.role !== 'authenticated')) {
+		router.push('/admin');
+		return null;
+	}
 	return (
 		<div className="container mx-auto p-4">
 			<WriteEditor />
