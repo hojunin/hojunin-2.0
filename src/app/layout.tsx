fcde9d772b7d539edee43554f773cce7f09b37cf
hojunin-29cm <hjinn@29cm.co.kr>
2024-09-28 17:49:08 +0900

    서치콘솔 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
 	title: '홈 | HJINN',
 	description: '프론트엔드 개발자 인호준의 개인 웹사이트입니다',
 	metadataBase: new URL('https://hojunin.com'),
+	verification: {
+		google: 'uwCYoBoSwTC9uFmHW4NboXtooElpmJxhrxozRvx3l-w',
+	},
 };
 
 export default function RootLayout({
@@ -26,6 +29,7 @@ export default function RootLayout({
 		<html lang="en">
 			<Clarity />
 			<link rel="apple-touch-icon" href="/apple-icon" type="image/png" sizes="180x180" />
+			<link rel="icon" href="/favicon.ico" />
 			<body className={inter.className}>
 				<ThemeProvider attribute="class" enableSystem defaultTheme="system">
 					<AmplitudeContextProvider>
