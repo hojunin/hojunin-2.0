87d13edd4f2e09c5507c672303d08ed905dd5ec9
hojunin-29cm <hjinn@29cm.co.kr>
2024-12-31 18:24:18 +0900

    PWA 세팅 오류 수정6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import BestContents from '@/components/home/best-contents';
 import NewContents from '@/components/home/new-contents';
 import HomeRollingCarousel from '@/components/home/home-rolling-carousel';
 import { PushNotificationManager } from '@/components/common/push-notification/push-notification-manager';
-import { InstallPrompt } from '@/components/common/push-notification/install-prompt';
 
 export default function Home() {
 	return (
