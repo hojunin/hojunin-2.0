7b60044eb98c3a4290fa14a85f93b8c0dd1d2c9c
hojunin <dlsghwns@naver.com>
2025-07-14 11:27:25 +0900

    Remove Push Notification functionality: Deleted action.ts and PushNotificationManager component to streamline the codebase and eliminate unused features.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import BestContents from '@/components/home/best-contents';
 import NewContents from '@/components/home/new-contents';
 import HomeRollingCarousel from '@/components/home/home-rolling-carousel';
-import { PushNotificationManager } from '@/components/common/push-notification/push-notification-manager';
 
 export default function Home() {
 	return (
@@ -13,10 +12,6 @@ export default function Home() {
 
 				<NewContents />
 			</div>
-
-			<>
-				<PushNotificationManager />
-			</>
 		</>
 	);
 }
